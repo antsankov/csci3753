bbd2d561733fd78a2df11c23b9cb137d8378780f
antsankov <antsankov@gmail.com>
2014-09-15 10:42:41 -0600

    rm files

--- assignment1/rkit/.rkit.ko.cmd
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-cmd_/home/user/csci3753/assignment1/rkit/rkit.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.11.0-12-generic/scripts/module-common.lds --build-id  -o /home/user/csci3753/assignment1/rkit/rkit.ko /home/user/csci3753/assignment1/rkit/rkit.o /home/user/csci3753/assignment1/rkit/rkit.mod.o
-=======
-cmd_/home/user/Documents/csci3753/assignment1/rkit/rkit.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.2.0-60-virtual/scripts/module-common.lds --build-id  -o /home/user/Documents/csci3753/assignment1/rkit/rkit.ko /home/user/Documents/csci3753/assignment1/rkit/rkit.o /home/user/Documents/csci3753/assignment1/rkit/rkit.mod.o
->>>>>>> 062865a0cf25ef9331420f976c34d326c8196f3e
