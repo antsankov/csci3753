d1ea4e811dc6524ca94c12fea2d9a9e42778c452
antsankov <antsankov@gmail.com>
2014-09-29 10:39:44 -0600

    Added to multi-lookup

--- assignment2/multi-lookup.c
+++ assignment2/multi-lookup.c
@@ -1,5 +1,38 @@
-#include<stdio.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-main(){
-	printf("Hello, the makefile works!");
+//right now this is hardcoded to 4, maybe find some way to check procs for e.c?
+#define NUM_THREADS 4
+
+int main(){
+	printf("Hello, this is the template!\n");
+	return 0;
 }
+
+//Rahat producer code:
+/*
+P{Produce();
+	wait(empty);
+	lock(lock);
+	===========
+	unlock(locl)
+	post(full)
+}
+
+e{
+	wait(full)
+	lock*lock
+	consume()
+	unlock(lock)
+	post(empty)
+}
+
+
+
+MultProdCon.c:
+
+sem_t empty = N;
+sem_t full = 0;
+mutex_t lock;
