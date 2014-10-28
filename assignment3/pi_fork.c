f96ca3c66f8feb55c4e006bdfe7896126f80409e
untra <untra.sam@gmail.com>
2014-10-28 12:43:18 -0600

    testscript made sexy

--- assignment3/pi_fork.c
+++ assignment3/pi_fork.c
@@ -54,28 +54,62 @@ int main(int argc, char* argv[]){
     }
 
     if (pid == 0){
-         //////////////WORK////////////
+         //////////////WORK////////////   
         long i;
         long iterations;
+        struct sched_param param;
+        int policy;
         double x, y;
         double inCircle = 0.0;
         double inSquare = 0.0;
         double pCircle = 0.0;
         double piCalc = 0.0;
-
-        /* Process program arguments to select iterations */
+    
+        /* Process program arguments to select iterations and policy */
         /* Set default iterations if not supplied */
         if(argc < 2){
         iterations = DEFAULT_ITERATIONS;
         }
-        /* Set iterations if supplied */
-        else{
-        iterations = atol(argv[1]);
-            if(iterations < 1){
-                fprintf(stderr, "Bad iterations value\n");
-                exit(EXIT_FAILURE);
-            }
+        /* Set default policy if not supplied */
+        if(argc < 3){
+        policy = SCHED_OTHER;
         }
+        /* Set iterations if supplied */
+        if(argc > 1){
+        iterations = atol(argv[1]);
+        if(iterations < 1){
+            fprintf(stderr, "Bad iterations value\n");
+            exit(EXIT_FAILURE);
+        }
+        }
+        /* Set policy if supplied */
+        if(argc > 2){
+        if(!strcmp(argv[2], "SCHED_OTHER")){
+            policy = SCHED_OTHER;
+        }
+        else if(!strcmp(argv[2], "SCHED_FIFO")){
+            policy = SCHED_FIFO;
+        }
+        else if(!strcmp(argv[2], "SCHED_RR")){
+            policy = SCHED_RR;
+        }
+        else{
+            fprintf(stderr, "Unhandeled scheduling policy\n");
+            exit(EXIT_FAILURE);
+        }
+        }
+        
+        /* Set process to max prioty for given scheduler */
+        param.sched_priority = sched_get_priority_max(policy);
+        
+        /* Set new scheduler policy */
+        fprintf(stdout, "Current Scheduling Policy: %d\n", sched_getscheduler(0));
+        fprintf(stdout, "Setting Scheduling Policy to: %d\n", policy);
+        if(sched_setscheduler(0, policy, &param)){
+        perror("Error setting scheduler policy");
+        exit(EXIT_FAILURE);
+        }
+        fprintf(stdout, "New Scheduling Policy: %d\n", sched_getscheduler(0));
 
         /* Calculate pi using statistical methode across all iterations*/
         for(i=0; i<iterations; i++){
